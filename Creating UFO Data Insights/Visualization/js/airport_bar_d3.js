3796e6cc614cdd6a2c5ef3c6d87065cb93dc7042
Soravis Taekasem <taekasem@usc.edu>
2018-07-01 13:50:25 -0700

    Updated chart script

---
+++ Creating UFO Data Insights/Visualization/js/airport_bar_d3.js
@@ -0,0 +1,59 @@
+var width = 800, height = 450;
+var svg = d3.select("#svg")
+	.attr("width", width)
+	.attr("height", height);
+
+var color = d3.scaleOrdinal()
+	.domain(["0-5", "5-10", "10-15", "15-20", "20+"])
+	.range(["#3498db", "#2ecc71", "#e74c3c", "#f1c40f", "#9b59b6"]);
+
+d3.json("../json/distance_airport.json", function(data)
+{
+	plot(data);
+});
+
+function plot(data)
+{
+	var max = d3.max(data, function (d) { return d["count"]; });
+	var y = d3.scaleLinear()
+		.domain([0, max])
+		.range([height - 50, 20]);
+	var x = d3.scaleBand()
+		.domain(["0-5", "5-10", "10-15", "15-20", "20+"])
+		.rangeRound([80, width - 50]);
+	var barWidth = (width - 100) / data.length;
+	var bar = svg.selectAll("g")
+		.data(data)
+		.enter()
+		.append("g");
+	bar.append("rect")
+		.attr("y", function(d) { return y(d["count"]); })
+		.attr("x", function(d) { return x(d["range"]); })
+		.attr("height", function(d) { return height - 50 - y(d["count"]); })
+		.attr("width", barWidth - 9)
+		.style("fill", function(d) { return color(d["range"]); });
+	var xAxis = d3.axisBottom()
+		.scale(x);
+	var yAxis = d3.axisLeft()
+		.scale(y);
+	svg.append("g")
+		.attr("transform", "translate(0," + (height - 50) + ")")
+		.call(xAxis);
+	svg.append("g")
+		.attr("transform", "translate(80, 0)")
+		.call(yAxis);
+	svg.append("text")
+		.attr("class", "small")
+		.attr("x", width/2 - 70)
+		.attr("y", height - 10)
+		.text("Distance from Airport (mi.)");
+	svg.append("text")
+		.attr("class", "small")
+		.attr("x", - height / 2)
+		.attr("y", 30)
+		.style("text-anchor", "middle")
+		.style("transform", "rotate(-90deg)")
+		.text("UFO Sightings");
+
+	return;
+}
